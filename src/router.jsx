2d0fcb6d110f60c07a03fb23d0927d648c33501e
raven-panda <ambroise.maire7@gmail.com>
2025-04-05 12:34:44 +0200

    add: created browser router

---
+++ src/router.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import IndexPage from './pages/Index.jsx';
+
+export default function AppRouter() {
+  console.log('rtouter');
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={'/'} index={true} element={<IndexPage />} />
+        <Route path={'*'} element={<h1>Not found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
