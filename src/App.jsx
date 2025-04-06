e65ef77ea5f003656d0b436a15c48373c30ae1f9
raven-panda <ambroise.maire7@gmail.com>
2025-04-06 11:02:49 +0200

    fix: fix css transition flashing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { ToastContainer } from 'react-toastify';
 import AppRouter from './router.jsx';
 import { AuthenticationProvider } from './components/hook/AuthHook.jsx';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    return () => document.body.classList.remove('disabled-transition');
+  }, []);
+
   return (
     <AuthenticationProvider>
       <ToastContainer />
