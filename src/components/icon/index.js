271b16cd501b3310c001509f337fc8067a0700f8
raven-panda <ambroise.maire7@gmail.com>
2025-04-06 14:59:02 +0200

    add: added icon components

---
+++ src/components/icon/index.js
@@ -0,0 +1,4 @@
+export { BellIcon } from './BellIcon.jsx';
+export { CommentBubbleIcon } from './CommentBubbleIcon.jsx';
+export { PeoplesIcon } from './PeoplesIcon.jsx';
+export { LinkIcon } from './LinkIcon.jsx';
